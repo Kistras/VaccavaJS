78d8196bf431e63d6554cc7f9b78cf384904d902
Kistras <81854722+Kistras@users.noreply.github.com>
2021-08-05 20:53:00 +0500

    BRANCHES

--- modules/testmessage.js
+++ modules/testmessage.js
@@ -1,7 +1,7 @@
 module.exports.start = function(client, dirpath) {
     client.on('message', msg => {
         if (msg.content === 'ping') {
-            msg.channel.send('I love bacon')
+            msg.channel.send('pong')
         }
     })
 }
